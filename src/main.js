68e373fd18186b494f5c1e478653eb819a796d24
SCrbnll <samuelcarbonell1203@gmail.com>
2024-12-22 21:50:15 +0100

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+
+document.querySelector('#app').innerHTML = `
+  <h1>EpubSphere</h1>
+`
+
